784c48fe63c3e4e28de00ee83128d1d0d6dbb576
Yash <yashkumarjmp2002@gmail.com>
2021-06-30 20:49:17 +0530

    Simple Guessing Game

---
+++ game.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include<time.h>
+
+using namespace std;
+
+int main(){
+   int n;
+
+   srand(time(0));
+   int c = (rand() % (20 + 1));
+
+   cout << "\n\n***** WELCOME TO NUMBER GUESSING GAME *****\n";
+
+   for(int i = 0; i < 6; i++){
+      if(i >= 0 && i < 5){
+         cout << "\nYou have " << 5 - i << " chances left.\n";
+         cout << "Enter your guess: ";
+         cin >> n;
+
+         if (n > c)
+            cout << "Sorry " << n << " is wrong. The number is less than that.\n";
+         else if (n < c)
+            cout << "Sorry " << n << " is wrong. The number is more than that.\n";
+         else if (n == c){
+            cout << "\n\nCongratulations. You guessed it!\n\n";
+            break;
+         }
+      }
+      else
+         cout << "\n\nSorry, You Lost!\n\n";
+   }
+
+return 0;
+}
+
